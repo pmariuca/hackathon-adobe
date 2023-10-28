f8fb557fda12c60ed216c9006c8e300bade46c18
Robert Pietroiu <96921061+PcGamerRo@users.noreply.github.com>
2023-10-28 17:06:15 +0300

    added extra features

--- react/resource-wise-react/src/pages/userprofile/userprofile.jsx
+++ react/resource-wise-react/src/pages/userprofile/userprofile.jsx
@@ -1,7 +1,28 @@
-function UserProfile(){
+import { useEffect } from 'react';
+import styles from './style.module.css'
+
+function UserProfile(props){
+
+    useEffect(()=>{
+        fetch(`http://localhost:3000/users/${1}`, {
+            method: "GET",
+            headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+    
+        }).then((response) => response.json())
+        .then((data) => console.log(data))
+    }, [])
+
     return (
         <>
-            <h1> User Profile page </h1>
+            <div className={styles.userMain}>
+                <div className={styles.userContainer}>
+                    <h1> User: {props.userEmail} </h1>
+                </div>        
+            </div>
         </>
     )
 }
