cb09784bf37220c2aca4711a6fe1f38aff390347
Robert Pietroiu <96921061+PcGamerRo@users.noreply.github.com>
2023-10-28 15:56:47 +0300

    localstorage credentials

--- react/resource-wise-react/src/pages/register/register.jsx
+++ react/resource-wise-react/src/pages/register/register.jsx
@@ -1,6 +1,10 @@
 import styles from './register.module.css'
 
+import { useNavigate } from 'react-router-dom'
+
+
 function Register() {
+    const navigate = useNavigate();    
 
     function reg(){
 
@@ -13,8 +17,29 @@ function Register() {
         else {
             fetch('http://localhost:3000/auth/register', {
                 method: "POST",
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                  },
                 body: JSON.stringify({email: email, password: password})
+            }).then(response => {
+                if (response.ok) {
+                    // the user was created
+
+                    localStorage.setItem("email", email)
+                    localStorage.setItem("password", password)
+                    navigate("/login")
+
+                } else {
+                    alert(response.status)
+                }
             })
+            .then(data => {
+                console.log(data); // Handle the response data here
+            })
+            .catch(error => {
+                console.log(error); // Handle errors here
+            });
         }
     }
 
